10bea146eaef2c888db216d80120740e095c3fb6
Colin Edwards <ColinJhEdwards@gmail.com>
2021-02-17 18:36:01 -0800

    added an animation that will reveal the home page on load

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ var githubIcon = document.querySelector(".githubIcon");
 var gitPreview = document.querySelector(".gitPreview");
 
 //Event Listeners
-githubIcon.addEventListener("mouseover", function () {
-  gitPreview.classList.remove("hide");
-  gitPreview.classList.add("anim");
-});
+// githubIcon.addEventListener("mouseover", function () {
+//   gitPreview.classList.remove("hide");
+//   gitPreview.classList.add("anim");
+// });
 
-githubIcon.addEventListener("mouseout", function () {
-  gitPreview.classList.add("hide");
-});
+// githubIcon.addEventListener("mouseout", function () {
+//   gitPreview.classList.add("hide");
+// });
 
 //Functions
+
+//Animations
+
+let controller;
+
+function animateHome() {
+  controller = new ScrollMagic.Controller();
+  const revealHome = document.querySelector(".revealHome");
+  const slideSetting = gsap.timeline({
+    defaults: { duration: 1, ease: "power2.inOut" },
+  });
+  slideSetting.fromTo(revealHome, { x: "0%" }, { x: "-100%" });
+}
+
+animateHome();
