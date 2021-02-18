f5c0cca758c02bd2cbaabc63532498eb1a904974
Colin Edwards <ColinJhEdwards@gmail.com>
2021-02-17 18:15:15 -0800

    Added scroll magic and gsap Cdn's

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+//Selectors
 var githubIcon = document.querySelector(".githubIcon");
 var gitPreview = document.querySelector(".gitPreview");
 
+//Event Listeners
 githubIcon.addEventListener("mouseover", function () {
   gitPreview.classList.remove("hide");
+  gitPreview.classList.add("anim");
 });
 
 githubIcon.addEventListener("mouseout", function () {
   gitPreview.classList.add("hide");
 });
+
+//Functions
