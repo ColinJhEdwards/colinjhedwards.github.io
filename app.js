0a8282a511c3a9e20c563808f8701095056e83df
Colin Edwards <ColinJhEdwards@gmail.com>
2021-02-18 08:30:37 -0800

    made it so the about me text will slide onto screen on load

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //Selectors
 var githubIcon = document.querySelector(".githubIcon");
 var gitPreview = document.querySelector(".gitPreview");
+let controller;
 
 //Event Listeners
 // githubIcon.addEventListener("mouseover", function () {
@@ -14,20 +15,18 @@ var gitPreview = document.querySelector(".gitPreview");
 
 //Functions
 
-//Animations
-
-let controller;
-
 function animateHome() {
   controller = new ScrollMagic.Controller();
   const revealHome = document.querySelector(".revealHome");
   const meImg = document.querySelector(".about-me-image");
+  const aboutText = document.querySelector(".about-me-text");
 
   const slideSetting = gsap.timeline({
     defaults: { duration: 1, ease: "power2.inOut" },
   });
   slideSetting.fromTo(revealHome, { x: "0%" }, { x: "-100%" });
   slideSetting.fromTo(meImg, { x: "110%" }, { x: "0%" }, "-=0.9");
+  slideSetting.fromTo(aboutText, { y: "80%" }, { y: "0%" }, "-=0.9");
 }
 
 animateHome();
